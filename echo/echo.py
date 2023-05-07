6abef1d191e796014c8fe2e14f30d73d2020ffa0
Rinco304 <75192177+Rinco304@users.noreply.github.com>
2023-05-07 17:08:45 +0800

    Update echo.py

--- echo/echo.py
+++ echo/echo.py
@@ -7,17 +7,27 @@ import hoshino
 from hoshino import R, Service, priv, util
 from nonebot import NoticeSession
 
-sv = Service('echo', enable_on_default=True)
+help_text='''
+这是一个桃按钮，收集了部分某holocn成员的b动静
+[@bot+我爱你]   taffy爱你哟
+[来点塔菲|来点taffy|来点cfm]   随机taffy能量
+[@bot+晚安]     来自cfm的晚安
+[想听生日歌]    送上cfm限定生日歌一首
+[爽死了]    爽死了
+[对呀对呀]      对呀对呀
+'''.strip()
+
+sv = Service('来点taffy', enable_on_default=True, help_=help_text)
 
 
-@sv.on_fullmatch(('我好了', 'whl'))
-async def wohaole(bot, event):
-    voice = R.get('record/echo/', 'whl.mp3')
-    rec = MessageSegment.record(f'file:///{os.path.abspath(voice.path)}')
-    await bot.send(event, rec)
+# @sv.on_fullmatch(('我好了', 'whl'))
+# async def wohaole(bot, event):
+#     voice = R.get('record/echo/', 'whl.mp3')
+#     rec = MessageSegment.record(f'file:///{os.path.abspath(voice.path)}')
+#     await bot.send(event, rec)
 
 
-@sv.on_fullmatch(('hso', '好涩噢', '好涩哦', '好色噢', '好色哦'))
+@sv.on_fullmatch('hso', '好涩噢', '好涩哦', '好色噢', '好色哦')
 async def hso(bot, event):
     if random.random() <= 0.2:
         voice = R.get('record/echo/', 'hso.mp3')
@@ -45,7 +55,7 @@ async def ywwuyidie(bot, event):
     await bot.send(event, rec)
 
 
-@sv.on_fullmatch('想听生日歌')
+@sv.on_fullmatch('想听生日歌', '今天我生日', '祝我生日快乐', '祝自己生日快乐', '生日快乐')
 async def shengrige(bot, event):
     voice = R.get('record/echo/', 'shengri.mp3')
     rec = MessageSegment.record(f'file:///{os.path.abspath(voice.path)}')
@@ -66,7 +76,7 @@ async def woaini(bot, event):
     await bot.send(event, rec)
 
 
-@sv.on_fullmatch(('晚安', '睡了', '睡觉了', '眠了'))
+@sv.on_fullmatch(('晚安', '睡了', '睡觉了', '眠了'), only_to_me=True)
 async def sleep(bot, event):
     if random.random() <= 0.3:
         voice = R.get('record/echo/', '晚安.mp3')
@@ -80,7 +90,7 @@ async def sleep(bot, event):
     await bot.send(event, rec)
 
 
-@sv.on_fullmatch(('来点桃宝', '来点echo', '来点cfm'))
+@sv.on_fullmatch(('来点塔菲', '来点taffy', '来点cfm'))
 async def random_echo(bot, event):
     echo_folder = R.get('record/echo/').path
     files = os.listdir(echo_folder)
